706c246211a12ea58ada62818f0c1f3702ff9224
Toby <toby.lawrance@gmail.com>
2020-11-30 16:38:21 +0000

    conversion to module

--- ParallelisationLibrary/ListExtensions.fs
+++ ParallelisationLibrary/ListExtensions.fs
@@ -2,15 +2,15 @@
 
     open System
     open System.Diagnostics.CodeAnalysis
+    
 
-    module Extensions =
-        let numThreads = Environment.ProcessorCount
-        type Microsoft.FSharp.Collections.List<'T> with
-        
+    module List =
+            let private numThreads = Environment.ProcessorCount
+            
             /// Maps a list to at most the number of logical Processors and then executes the function
             /// The mapping function 'T -> 'U is applied to each element
             /// returns a 'U list in the same order as input
-            static member pMap (mapper: 'T -> 'U) (l: 'T list) : 'U list =
+            let pMap (mapper: 'T -> 'U) (l: 'T list) : 'U list =
                 let asyncMap f xl = async{return List.map f xl}
                 l
                 |> List.splitInto numThreads
@@ -23,7 +23,7 @@
             /// Maps two lists to at most the number of logical Processors and then executes the function
             /// The mapping function 'T -> 'T2 -> 'U is applied to each element
             /// returns a 'U list in the same order as input
-            static member pMap2 (mapper: 'T -> 'T2 -> 'U) (l: 'T list) (l2: 'T2 list) : 'U list =
+            let pMap2 (mapper: 'T -> 'T2 -> 'U) (l: 'T list) (l2: 'T2 list) : 'U list =
                 let asyncMap f xl yl = async{return List.map2 f xl yl}
                 let prepared l = List.splitInto numThreads l
                 List.map2 (asyncMap mapper) (prepared l) (prepared l2)
@@ -36,7 +36,7 @@
             /// Maps three lists to at most the number of logical Processors and then executes the function
             /// The mapping function 'T -> 'T2 -> 'T3 -> 'U is applied to each element
             /// returns a 'U list in the same order as input
-            static member pMap3 (mapper: 'T -> 'T2 -> 'T3 -> 'U) (l: 'T list) (l2: 'T2 list) (l3: 'T3 list) : 'U list =
+            let pMap3 (mapper: 'T -> 'T2 -> 'T3 -> 'U) (l: 'T list) (l2: 'T2 list) (l3: 'T3 list) : 'U list =
                 let asyncMap f xl yl zl = async{return List.map3 f xl yl zl}
                 let prepared l = List.splitInto numThreads l
                 List.map3 (asyncMap mapper) (prepared l) (prepared l2) (prepared l3)
@@ -48,7 +48,7 @@
                 
             /// Same as List.init but in a parallel fashion
             /// Appears to run significantly faster under most loads
-            static member pInit (n: int) (mapper: int -> 'U) : 'U list =
+            let pInit (n: int) (mapper: int -> 'U) : 'U list =
                 let asyncMap f count = async{return List.init count f}
                 let applyOffset off (f : int -> 'U) n = f (off + n)  
                 let genRanges x n =
@@ -72,7 +72,7 @@
             /// Reduces a list to a value in a parallel fashion NOTE: non-associative functions will have strange results. Does seem to be slower than single threaded for trivial operations
             /// The reducing function SHOULD BE ASSOCIATIVE
             /// Returns a single value of type 'T
-            static member pReduce (reducer: 'T -> 'T -> 'T) (l: 'T list): 'T =
+            let pReduce (reducer: 'T -> 'T -> 'T) (l: 'T list): 'T =
                 let asyncReduce f xl = async{return List.reduce f xl}
                 l
                 |> List.splitInto numThreads
