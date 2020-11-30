71573793e52611a785b36e870107076573f55eb8
Toby <toby.lawrance@gmail.com>
2020-11-30 16:46:16 +0000

    Reformatting

--- ParallelisationLibrary/ArrayExtensions.fs
+++ ParallelisationLibrary/ArrayExtensions.fs
@@ -1,51 +1,49 @@
 ﻿namespace ParallelisationLibrary
 
-    open System    
+open System
 
-    module Array =
-            let private numThreads = Environment.ProcessorCount
-            
-            /// Uses the existing Array Parallel extension
-            let pMap (mapper: 'T -> 'U) (l: 'T array) : 'U array =
-                Array.Parallel.map mapper l
-                
-            /// Maps two Arrays to at most the number of logical Processors and then executes the function
-            /// The mapping function 'T -> 'T2 -> 'U is applied to each element
-            /// returns a 'U Array in the same order as input
-            let pMap2 (mapper: 'T -> 'T2 -> 'U) (l: 'T array) (l2: 'T2 array) : 'U array =
-                let asyncMap f xl yl = async{return Array.map2 f xl yl}
-                let prepared l = Array.splitInto numThreads l
-                Array.map2 (asyncMap mapper) (prepared l) (prepared l2)
-                |> Async.Parallel
-                |> Async.RunSynchronously
-                |> Array.concat
-                
-                
-            /// Maps three Arrays to at most the number of logical Processors and then executes the function
-            /// The mapping function 'T -> 'T2 -> 'T3 -> 'U is applied to each element
-            /// returns a 'U Array in the same order as input
-            let pMap3 (mapper: 'T -> 'T2 -> 'T3 -> 'U) (l: 'T array) (l2: 'T2 array) (l3: 'T3 array) : 'U array =
-                let asyncMap f xl yl zl = async{return Array.map3 f xl yl zl}
-                let prepared l = Array.splitInto numThreads l
-                Array.map3 (asyncMap mapper) (prepared l) (prepared l2) (prepared l3)
-                |> Async.Parallel
-                |> Async.RunSynchronously
-                |> Array.concat
-                
-                
-            /// Uses existing Parallel extension for initialisation
-            let pInit (n: int) (mapper: int -> 'U) : 'U array =
-                Array.Parallel.init n mapper
-                
-                
-            /// Reduces a Array to a value in a parallel fashion NOTE: non-associative functions will have strange results. Does seem to be slower than single threaded for trivial operations
-            /// The reducing function SHOULD BE ASSOCIATIVE
-            /// Returns a single value of type 'T
-            let pReduce (reducer: 'T -> 'T -> 'T) (l: 'T array): 'T =
-                let asyncReduce f xl = async{return Array.reduce f xl}
-                l
-                |> Array.splitInto numThreads
-                |> Array.map (asyncReduce reducer)
-                |> Async.Parallel
-                |> Async.RunSynchronously
-                |> Array.reduce reducer
+module Array =
+    let private numThreads = Environment.ProcessorCount
+
+    /// Uses the existing Array Parallel extension
+    let pMap (mapper: 'T -> 'U) (l: 'T array): 'U array = Array.Parallel.map mapper l
+
+    /// Maps two Arrays to at most the number of logical Processors and then executes the function
+    /// The mapping function 'T -> 'T2 -> 'U is applied to each element
+    /// returns a 'U Array in the same order as input
+    let pMap2 (mapper: 'T -> 'T2 -> 'U) (l: 'T array) (l2: 'T2 array): 'U array =
+        let asyncMap f xl yl = async { return Array.map2 f xl yl }
+        let prepared l = Array.splitInto numThreads l
+        Array.map2 (asyncMap mapper) (prepared l) (prepared l2)
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> Array.concat
+
+
+    /// Maps three Arrays to at most the number of logical Processors and then executes the function
+    /// The mapping function 'T -> 'T2 -> 'T3 -> 'U is applied to each element
+    /// returns a 'U Array in the same order as input
+    let pMap3 (mapper: 'T -> 'T2 -> 'T3 -> 'U) (l: 'T array) (l2: 'T2 array) (l3: 'T3 array): 'U array =
+        let asyncMap f xl yl zl = async { return Array.map3 f xl yl zl }
+        let prepared l = Array.splitInto numThreads l
+        Array.map3 (asyncMap mapper) (prepared l) (prepared l2) (prepared l3)
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> Array.concat
+
+
+    /// Uses existing Parallel extension for initialisation
+    let pInit (n: int) (mapper: int -> 'U): 'U array = Array.Parallel.init n mapper
+
+
+    /// Reduces a Array to a value in a parallel fashion NOTE: non-associative functions will have strange results. Does seem to be slower than single threaded for trivial operations
+    /// The reducing function SHOULD BE ASSOCIATIVE
+    /// Returns a single value of type 'T
+    let pReduce (reducer: 'T -> 'T -> 'T) (l: 'T array): 'T =
+        let asyncReduce f xl = async { return Array.reduce f xl }
+        l
+        |> Array.splitInto numThreads
+        |> Array.map (asyncReduce reducer)
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> Array.reduce reducer
--- ParallelisationLibrary/ListExtensions.fs
+++ ParallelisationLibrary/ListExtensions.fs
@@ -1,80 +1,86 @@
 ﻿namespace ParallelisationLibrary
 
-    open System
-  
-    module List =
-            let private numThreads = Environment.ProcessorCount
-            
-            /// Maps a list to at most the number of logical Processors and then executes the function
-            /// The mapping function 'T -> 'U is applied to each element
-            /// returns a 'U list in the same order as input
-            let pMap (mapper: 'T -> 'U) (l: 'T list) : 'U list =
-                let asyncMap f xl = async{return List.map f xl}
-                l
-                |> List.splitInto numThreads
-                |> List.map (asyncMap mapper)
-                |> Async.Parallel
-                |> Async.RunSynchronously
-                |> Array.toList
-                |> List.concat
-                
-            /// Maps two lists to at most the number of logical Processors and then executes the function
-            /// The mapping function 'T -> 'T2 -> 'U is applied to each element
-            /// returns a 'U list in the same order as input
-            let pMap2 (mapper: 'T -> 'T2 -> 'U) (l: 'T list) (l2: 'T2 list) : 'U list =
-                let asyncMap f xl yl = async{return List.map2 f xl yl}
-                let prepared l = List.splitInto numThreads l
-                List.map2 (asyncMap mapper) (prepared l) (prepared l2)
-                |> Async.Parallel
-                |> Async.RunSynchronously
-                |> Array.toList
-                |> List.concat
-                
-                
-            /// Maps three lists to at most the number of logical Processors and then executes the function
-            /// The mapping function 'T -> 'T2 -> 'T3 -> 'U is applied to each element
-            /// returns a 'U list in the same order as input
-            let pMap3 (mapper: 'T -> 'T2 -> 'T3 -> 'U) (l: 'T list) (l2: 'T2 list) (l3: 'T3 list) : 'U list =
-                let asyncMap f xl yl zl = async{return List.map3 f xl yl zl}
-                let prepared l = List.splitInto numThreads l
-                List.map3 (asyncMap mapper) (prepared l) (prepared l2) (prepared l3)
-                |> Async.Parallel
-                |> Async.RunSynchronously
-                |> Array.toList
-                |> List.concat
-                
-                
-            /// Same as List.init but in a parallel fashion
-            /// Appears to run significantly faster under most loads
-            let pInit (n: int) (mapper: int -> 'U) : 'U list =
-                let asyncMap f count = async{return List.init count f}
-                let applyOffset off (f : int -> 'U) n = f (off + n)  
-                let genRanges x n =
-                    let spares = n % x
-                    let each = n / x
-                    let rec genRanges' don spare acc =
-                        let added = (don + each + (if spare > 0 then 1 else 0))
-                        match don with
-                        | _ when (added) >= n -> List.append acc [(don,added-don)]
-                        | _ -> genRanges' added (spare-1) (List.append acc [(don,added-don)])
-                    genRanges' 0 spares []
-                let ranges = genRanges numThreads n
-                ranges
-                |> List.map (fun (off,count) -> asyncMap (applyOffset off mapper) count)
-                |> Async.Parallel
-                |> Async.RunSynchronously
-                |> Array.toList
-                |> List.concat
-                
-                
-            /// Reduces a list to a value in a parallel fashion NOTE: non-associative functions will have strange results. Does seem to be slower than single threaded for trivial operations
-            /// The reducing function SHOULD BE ASSOCIATIVE
-            /// Returns a single value of type 'T
-            let pReduce (reducer: 'T -> 'T -> 'T) (l: 'T list): 'T =
-                let asyncReduce f xl = async{return List.reduce f xl}
-                l
-                |> List.splitInto numThreads
-                |> List.map (asyncReduce reducer)
-                |> Async.Parallel
-                |> Async.RunSynchronously
-                |> Array.reduce reducer
+open System
+
+module List =
+    let private numThreads = Environment.ProcessorCount
+
+    /// Maps a list to at most the number of logical Processors and then executes the function
+    /// The mapping function 'T -> 'U is applied to each element
+    /// returns a 'U list in the same order as input
+    let pMap (mapper: 'T -> 'U) (l: 'T list): 'U list =
+        let asyncMap f xl = async { return List.map f xl }
+        l
+        |> List.splitInto numThreads
+        |> List.map (asyncMap mapper)
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> Array.toList
+        |> List.concat
+
+    /// Maps two lists to at most the number of logical Processors and then executes the function
+    /// The mapping function 'T -> 'T2 -> 'U is applied to each element
+    /// returns a 'U list in the same order as input
+    let pMap2 (mapper: 'T -> 'T2 -> 'U) (l: 'T list) (l2: 'T2 list): 'U list =
+        let asyncMap f xl yl = async { return List.map2 f xl yl }
+        let prepared l = List.splitInto numThreads l
+        List.map2 (asyncMap mapper) (prepared l) (prepared l2)
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> Array.toList
+        |> List.concat
+
+
+    /// Maps three lists to at most the number of logical Processors and then executes the function
+    /// The mapping function 'T -> 'T2 -> 'T3 -> 'U is applied to each element
+    /// returns a 'U list in the same order as input
+    let pMap3 (mapper: 'T -> 'T2 -> 'T3 -> 'U) (l: 'T list) (l2: 'T2 list) (l3: 'T3 list): 'U list =
+        let asyncMap f xl yl zl = async { return List.map3 f xl yl zl }
+        let prepared l = List.splitInto numThreads l
+        List.map3 (asyncMap mapper) (prepared l) (prepared l2) (prepared l3)
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> Array.toList
+        |> List.concat
+
+
+    /// Same as List.init but in a parallel fashion
+    /// Appears to run significantly faster under most loads
+    let pInit (n: int) (mapper: int -> 'U): 'U list =
+        let asyncMap f count = async { return List.init count f }
+        let applyOffset off (f: int -> 'U) n = f (off + n)
+
+        let genRanges x n =
+            let spares = n % x
+            let each = n / x
+
+            let rec genRanges' don spare acc =
+                let added =
+                    (don + each + (if spare > 0 then 1 else 0))
+
+                match don with
+                | _ when (added) >= n -> List.append acc [ (don, added - don) ]
+                | _ -> genRanges' added (spare - 1) (List.append acc [ (don, added - don) ])
+
+            genRanges' 0 spares []
+
+        let ranges = genRanges numThreads n
+        ranges
+        |> List.map (fun (off, count) -> asyncMap (applyOffset off mapper) count)
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> Array.toList
+        |> List.concat
+
+
+    /// Reduces a list to a value in a parallel fashion NOTE: non-associative functions will have strange results. Does seem to be slower than single threaded for trivial operations
+    /// The reducing function SHOULD BE ASSOCIATIVE
+    /// Returns a single value of type 'T
+    let pReduce (reducer: 'T -> 'T -> 'T) (l: 'T list): 'T =
+        let asyncReduce f xl = async { return List.reduce f xl }
+        l
+        |> List.splitInto numThreads
+        |> List.map (asyncReduce reducer)
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> Array.reduce reducer
