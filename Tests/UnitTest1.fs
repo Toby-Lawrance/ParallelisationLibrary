7760c69fc024f740be12dba91e978376de602bbe
Toby <toby.lawrance@gmail.com>
2020-11-30 16:56:41 +0000

    Additional tests and renaming of old tests

--- Tests/UnitTest1.fs
+++ Tests/UnitTest1.fs
@@ -19,14 +19,28 @@ let timeIt name (thing: 'X -> 'Y) arg =
 let Setup () = ()
 
 [<Test>]
-let CalltMap () =
+let ``Basic List.pMap usage`` () =
     let numsToTransform = [ 1 .. 1000 ]
     let floatList = [ 1.0 .. 1000.0 ]
     let floated = List.pMap float numsToTransform
     Assert.AreEqual(floated, floatList)
 
 [<Test>]
-let SerialVsParallelBenchMark () =
+let ``Basic Seq.pMap usage`` () =
+    let numsToTransform = [ 1 .. 1000 ]
+    let floatList = [ 1.0 .. 1000.0 ]
+    let floated = Seq.pMap float numsToTransform
+    Assert.AreEqual(floated, floatList)
+
+[<Test>]
+let ``Basic Array.pMap usage`` () =
+    let numsToTransform = [| 1 .. 1000 |]
+    let floatList = [| 1.0 .. 1000.0 |]
+    let floated = Array.pMap float numsToTransform
+    Assert.AreEqual(floated, floatList)
+
+[<Test>]
+let ``List pMap Serial Vs Parallel BenchMark`` () =
     let task x =
         List.reduce (*) (List.map (fun y -> (x |> float) ** (y |> float)) [ 1 .. 100000 ])
 
@@ -36,7 +50,30 @@ let SerialVsParallelBenchMark () =
     Assert.LessOrEqual(timeParallel, timeSerial)
 
 [<Test>]
-let RangeVsMapBenchmark () =
+let ``Seq pMap Serial Vs Parallel BenchMark`` () =
+    let task x =
+        Seq.reduce (*) (Seq.map (fun y -> (x |> float) ** (y |> float)) [ 1 .. 100000 ])
+
+    let range = [ 1 .. 100 ]
+    let timeSerial = timeIt "Serial" (Seq.map task) range
+    let timeParallel = timeIt "Parallel" (Seq.pMap task) range
+    Assert.LessOrEqual(timeParallel, timeSerial)
+
+[<Test>]
+let ``Array pMap Serial Vs Parallel BenchMark`` () =
+    let task x =
+        Array.reduce (*) (Array.map (fun y -> (x |> float) ** (y |> float)) [| 1 .. 100000 |])
+
+    let range = [| 1 .. 100 |]
+    let timeSerial = timeIt "Serial" (Array.map task) range
+
+    let timeParallel =
+        timeIt "Parallel" (Array.pMap task) range
+
+    Assert.LessOrEqual(timeParallel, timeSerial)
+
+[<Test>]
+let ``List pInit Vs pMap Benchmark`` () =
     let task x =
         List.reduce (*) (List.map (fun y -> (x |> float) ** (y |> float)) [ 1 .. 100 ])
 
@@ -44,6 +81,29 @@ let RangeVsMapBenchmark () =
         timeIt "Map" (List.pMap task) [ 1 .. 1000000 ]
 
     let timeRange = timeIt "Range" (List.pInit 100000) task
+    Assert.LessOrEqual(timeRange, timeMap)
+
+[<Test>]
+let ``Seq pInit Vs pMap Benchmark`` () =
+    let task x =
+        Seq.reduce (*) (Seq.map (fun y -> (x |> float) ** (y |> float)) [ 1 .. 100 ])
+
+    let timeMap =
+        timeIt "Map" (Seq.pMap task) [ 1 .. 1000000 ]
+
+    let timeRange = timeIt "Range" (Seq.pInit 100000) task
+    Assert.LessOrEqual(timeRange, timeMap)
+
+
+[<Test>]
+let ``Array pInit Vs pMap Benchmark`` () =
+    let task x =
+        Array.reduce (*) (Array.map (fun y -> (x |> float) ** (y |> float)) [| 1 .. 100 |])
+
+    let timeMap =
+        timeIt "Map" (Array.pMap task) [| 1 .. 1000000 |]
+
+    let timeRange = timeIt "Range" (Array.pInit 100000) task
     Assert.LessOrEqual(timeRange, timeMap)
 
 [<Test>]
@@ -56,36 +116,25 @@ let ``Show Correct offsets by generating a sequential list`` () =
     Assert.AreEqual(list, normalList)
 
 [<Test>]
-let rangeGenVsSplit () =
-    let num = 100000
-
-    let genRanges x n =
-        let spares = n % x
-        let each = n / x
-
-        let rec genRanges' don spare acc =
-            let added =
-                (don + each + (if spare > 0 then 1 else 0))
-
-            let range = [ (don + 1) .. added ]
-            match don with
-            | _ when (added) >= n -> List.append acc [ range ]
-            | _ -> genRanges' added (spare - 1) (List.append acc [ range ])
-
-        genRanges' 0 spares []
-
-    let range = [ 1 .. num ]
-
-    let timeSplit =
-        timeIt "Split" (List.splitInto numThreads) range
-
-    let timeRange =
-        timeIt "Range" (genRanges numThreads) num
-
-    Assert.LessOrEqual(timeRange, timeSplit)
+let ``Show Correct offsets by generating a sequential seq`` () =
+    let task x = x
+    let list = Seq.pInit 100000 task
+    printfn ""
+    printfn ""
+    let normalList = Seq.init 100000 task
+    Assert.AreEqual(list, normalList)
 
 [<Test>]
-let genIsSplit () =
+let ``Show Correct offsets by generating a sequential array`` () =
+    let task x = x
+    let list = Array.pInit 100000 task
+    printfn ""
+    printfn ""
+    let normalList = Array.init 100000 task
+    Assert.AreEqual(list, normalList)
+
+[<Test>]
+let ``Range generation code is equivalent to Split`` () =
     let genRanges x n =
         let spares = n % x
         let each = n / x
